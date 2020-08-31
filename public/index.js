2abc6ecb183f0a74ad1ef699003cb03ed7bf6701
Christian Paul <info@jaller.de>
2020-09-01 01:58:27 +0200

    Day 2

---
+++ public/index.js
@@ -0,0 +1,272 @@
+const display = document.getElementById('display');
+let track;
+let player;
+let raceStartTime;
+
+let pipes = [];
+
+function addPipe(track, userId, name = "You") {
+    const root = document.createElement('p');
+    const div = document.createElement('div');
+    div.innerText = name;
+    const progress = document.createElement('progress');
+    progress.value = 0;
+    progress.classList.add('pipe');
+    root.append(div);
+    root.append(progress);
+    document.getElementById('pipes').append(root);
+    const player = new Player(track, userId);
+    pipes.push({
+        player,
+        progress,
+        root,
+    });
+    return player;
+}
+
+function removePipe(userId) {
+    const pipe = pipes.find(p => p.player.id === userId);
+    pipe.root.remove();
+    const pos = pipes.findIndex(p => p === pipe);
+    pipes.splice(pos, 1);
+}
+
+window.addEventListener('keypress', (event) => {
+    if (event.key === 'r') {
+        console.log('restar');
+        socket.emit('restar');
+    }
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === ' ') {
+        const time = Date.now() - raceStartTime;
+        player.press(time);
+    }
+});
+
+window.addEventListener('pointerdown', (event) => {
+    const time = Date.now() - raceStartTime;
+    player.press(time);
+});
+
+window.addEventListener('pointerup', (event) => {
+    const time = Date.now() - raceStartTime;
+    player.release(time);
+});
+
+window.addEventListener('keyup', (event) => {
+    if (event.key === ' ') {
+        const time = Date.now() - raceStartTime;
+        player.release(time);
+    }
+});
+
+setInterval(() => {
+    if (raceStartTime) {
+        const time = Date.now() - raceStartTime;
+        const position = player.getPosition(time);
+        const next = player.getNextChange(time);
+        display.innerHTML = (track.isBump(position) ? '🟥' : '🟩') + ` next: ${next.type} at ${next.position}; whenWillIbethere: ${player.whenWillIbethere(next.position)}`;
+        pipes.forEach(pipe => {
+            pipe.progress.value = pipe.player.getPosition(time);
+        });
+    }
+}, 100);
+
+class Track {
+    constructor() {
+        this.length = 1000;
+        this.bumps = [];
+        for (let index = 1; index < 10; index++) {
+            this.bumps.push({
+                start: index * 100 + 100,
+                length: 50,
+            });
+        }
+    }
+
+    isBump(position) {
+        return this.bumps.some((bump) => (
+            position >= bump.start && position <= bump.start + bump.length
+        ));
+    }
+}
+
+class Player {
+    constructor(track, id) {
+        this.id = id;
+        this.track = track;
+        this.changes = [];
+        this.isDown = false;
+    }
+
+    addMovementEvent(change) {
+        this.changes.push(change);
+    }
+
+    press(timestamp) {
+        if (this.isDown) return;
+        this.isDown = true;
+        const position = this.getPosition(timestamp);
+        const change = {
+            time: timestamp,
+            position,
+            speed: this.track.isBump(position) ? 0.005 : 0.04,
+        };
+        this.addMovementEvent(change);
+        socket.emit("movement", change);
+    }
+
+    release(timestamp) {
+        if (!this.isDown) return;
+        this.isDown = false;
+        const position = this.getPosition(timestamp);
+        const change = {
+            time: timestamp,
+            position,
+            speed: 0.01,
+        };
+        this.addMovementEvent(change);
+        socket.emit("movement", change);
+    }
+
+    get lastChange() {
+        if (this.changes.length === 0) {
+            return null;
+        }
+        return this.changes[this.changes.length - 1];
+    }
+
+    whenWillIbethere(target) {
+        const deltaLength = this.lastChange ? target - this.lastChange.position : 0;
+        const speed = this.lastChange ? this.lastChange.speed : 0.01;
+        return deltaLength / speed + (this.lastChange ? this.lastChange.time : 0);
+    }
+
+    getNextChange(timestamp) {
+        const position = this.getPosition(timestamp);
+        const finish = this.track.length;
+        const nextBumpStart = (this.track.bumps.find((bump) => (
+            position < bump.start
+        )) || {}).start || finish + 1;
+        const nextBumpEndBump = this.track.bumps.find((bump) => (
+            position < bump.start + bump.length
+        ));
+        const nextBumpEnd = nextBumpEndBump ? nextBumpEndBump.start + nextBumpEndBump.length : finish + 1;
+        if (nextBumpStart < finish || nextBumpEnd < finish) {
+            if (nextBumpStart < nextBumpEnd) {
+                return {
+                    position: nextBumpStart,
+                    type: 'bumpstart',
+                };
+            } else {
+                return {
+                    position: nextBumpEnd,
+                    type: 'bumpend',
+                };
+            }
+        }
+        return {
+            position: finish,
+            type: 'finish',
+        };
+    }
+
+    getPosition(timestamp) {
+        if (this.lastChange) {
+            const delta = timestamp - this.lastChange.time;
+            return this.lastChange.position + delta * this.lastChange.speed;
+        }
+        return timestamp * 0.01;
+    }
+}
+
+function setMessage(text) {
+    document.getElementById('message').innerText = text;
+}
+
+function displayScore(text) {
+    document.getElementById('score').innerText = text;
+}
+
+const points = {
+    win: 0,
+    lose: 0,
+    draw: 0,
+}
+
+/**
+ * Binde Socket.IO and button events
+ */
+function bind() {
+
+    socket.on("start", (time) => {
+        setMessage("Race started");
+        raceStartTime = time;
+        pipes.forEach(pipe => {
+            pipe.player.changes = [];
+            pipe.progress.max = track.length;
+            pipe.progress.value = 0;
+        });
+    });
+
+    socket.on("win", () => {
+        points.win++;
+        displayScore("You win!");
+    });
+
+    socket.on("lose", () => {
+        points.lose++;
+        displayScore("You lose!");
+    });
+
+    socket.on("draw", () => {
+        points.draw++;
+        displayScore("Draw!");
+    });
+
+    socket.on("end", () => {
+        setMessage("Waiting for opponent...");
+    });
+
+    socket.on("joined", (id, name) => {
+        setMessage(`Player ${name} joined.`);
+        addPipe(track, id, name);
+    });
+
+    socket.on("left", (id) => {
+        setMessage(`Player ${id} left.`);
+        removePipe(id);
+    });
+
+    socket.on("movement", (id, movement) => {
+        pipes.find(pipe => pipe.player.id === id).player.addMovementEvent(movement);
+    });
+
+    socket.on("connect", () => {
+        setMessage("Waiting for opponent...");
+    });
+
+    socket.on("disconnect", () => {
+        setMessage("Connection lost!");
+    });
+
+    socket.on("error", () => {
+        setMessage("Connection error!");
+    });
+}
+
+raceStartTime = Date.now();
+track = new Track();
+player = addPipe(track);
+
+/**
+ * Client module init
+ */
+function init() {
+    socket = io({ upgrade: false, transports: ["websocket"] });
+    bind();
+}
+
+window.addEventListener("load", init, false);
--- public/server.js
+++ public/server.js
@@ -1,88 +1,58 @@
 "use strict";
-
-/**
- * User sessions
- * @param {Array} users
- */
-const users = [];
-
 /**
  * Find opponent for a user
  * @param {User} user
  */
-function findOpponent(user) {
-	for (let i = 0; i < users.length; i++) {
-		if (
-			user !== users[i] &&
-			users[i].opponent === null
-		) {
-			new Game(user, users[i]).start();
-		}
-	}
+function findGame(user) {
+	game.addUser(user);
+	user.game = game;
 }
 
-/**
- * Remove user session
- * @param {User} user
- */
-function removeUser(user) {
-	users.splice(users.indexOf(user), 1);
+function pickRandom(array) {
+	return array[Math.floor(Math.random() * array.length)];
 }
 
 /**
  * Game class
  */
 class Game {
-
-	/**
-	 * @param {User} user1
-	 * @param {User} user2
-	 */
-	constructor(user1, user2) {
-		this.user1 = user1;
-		this.user2 = user2;
+	constructor() {
+		this.users = [];
 	}
 
-	/**
-	 * Start new game
-	 */
+	addUser(newUser) {
+		this.users.forEach((user) => {
+			newUser.socket.emit('joined', user.id, user.name);
+			user.socket.emit('joined', newUser.id, newUser.name);
+		});
+		this.users.push(newUser);
+	}
+
+	removeUser(user) {
+		const pos = this.users.findIndex(u => u === user);
+		this.users.splice(pos, 1);
+		this.users.forEach(u => {
+			u.socket.emit('left', user.id);
+		});
+	}
+
+	populateMovement(user, movement) {
+		console.log(movement);
+		this.users.filter(u => u.id !== user.id).forEach((u) => {
+			u.socket.emit('movement', user.id, movement);
+		});
+	}
+
 	start() {
-		this.user1.start(this, this.user2);
-		this.user2.start(this, this.user1);
+		const time = Date.now();
+		this.users.forEach((user) => {
+			user.start(time);
+		});
 	}
 
-	/**
-	 * Is game ended
-	 * @return {boolean}
-	 */
-	ended() {
-		return this.user1.guess !== GUESS_NO && this.user2.guess !== GUESS_NO;
+	end() {
+		this.start();
 	}
-
-	/**
-	 * Final score
-	 */
-	score() {
-		if (
-			this.user1.guess === GUESS_ROCK && this.user2.guess === GUESS_SCISSORS ||
-			this.user1.guess === GUESS_PAPER && this.user2.guess === GUESS_ROCK ||
-			this.user1.guess === GUESS_SCISSORS && this.user2.guess === GUESS_PAPER
-		) {
-			this.user1.win();
-			this.user2.lose();
-		} else if (
-			this.user2.guess === GUESS_ROCK && this.user1.guess === GUESS_SCISSORS ||
-			this.user2.guess === GUESS_PAPER && this.user1.guess === GUESS_ROCK ||
-			this.user2.guess === GUESS_SCISSORS && this.user1.guess === GUESS_PAPER
-		) {
-			this.user2.win();
-			this.user1.lose();
-		} else {
-			this.user1.draw();
-			this.user2.draw();
-		}
-	}
-
 }
 
 /**
@@ -93,48 +63,27 @@ class User {
 	/**
 	 * @param {Socket} socket
 	 */
-	constructor(socket) {
+	constructor(socket, game) {
 		this.socket = socket;
+		this.id = socket.id;
 		this.game = null;
-		this.opponent = null;
-		this.guess = GUESS_NO;
+		this.name = pickRandom('Paprika,Tomate,Zwiebel,Lauch,Aubergine,Avocado,Orange,Apfel,Birne,Erdbere'.split(','));
+		this.movements = [];
 	}
 
-	/**
-	 * Set guess value
-	 * @param {number} guess
-	 */
-	setGuess(guess) {
-		if (
-			!this.opponent ||
-			guess <= GUESS_NO ||
-			guess > GUESS_SCISSORS
-		) {
-			return false;
-		}
-		this.guess = guess;
-		return true;
+	addMovementEvent(event) {
+		this.movements.push(event);
+		this.game.populateMovement(this, event);
 	}
 
-	/**
-	 * Start new game
-	 * @param {Game} game
-	 * @param {User} opponent
-	 */
-	start(game, opponent) {
-		this.game = game;
-		this.opponent = opponent;
-		this.guess = GUESS_NO;
-		this.socket.emit("start");
+	start(time) {
+		this.socket.emit("start", time);
 	}
 
 	/**
 	 * Terminate game
 	 */
 	end() {
-		this.game = null;
-		this.opponent = null;
-		this.guess = GUESS_NO;
 		this.socket.emit("end");
 	}
 
@@ -142,21 +91,21 @@ class User {
 	 * Trigger win event
 	 */
 	win() {
-		this.socket.emit("win", this.opponent.guess);
+		this.socket.emit("win");
 	}
 
 	/**
 	 * Trigger lose event
 	 */
 	lose() {
-		this.socket.emit("lose", this.opponent.guess);
+		this.socket.emit("lose");
 	}
 
 	/**
 	 * Trigger draw event
 	 */
 	draw() {
-		this.socket.emit("draw", this.opponent.guess);
+		this.socket.emit("draw");
 	}
 
 }
@@ -169,26 +118,23 @@ module.exports = {
 
 	io: (socket) => {
 		const user = new User(socket);
-		users.push(user);
-		findOpponent(user);
+		findGame(user);
 
 		socket.on("disconnect", () => {
+			game.removeUser(user);
 			console.log("Disconnected: " + socket.id);
-			removeUser(user);
-			if (user.opponent) {
-				user.opponent.end();
-				findOpponent(user.opponent);
-			}
 		});
 
-		socket.on("guess", (guess) => {
-			console.log("Guess: " + socket.id);
-			if (user.setGuess(guess) && user.game.ended()) {
-				user.game.score();
-				user.game.start();
-				storage.get('games', 0).then(games => {
-					storage.set('games', games + 1);
-				});
+		socket.on("restar", () => {
+			console.log("Restarted: " + socket.id);
+			user.game.start();
+		});
+
+		socket.on("movement", (event) => {
+			console.log("Movement: " + socket.id);
+			user.addMovementEvent(event);
+			if (event.position >= 1000) {
+				user.game.end(user);
 			}
 		});
 
@@ -202,3 +148,5 @@ module.exports = {
 	}
 
 };
+
+const game = new Game();
